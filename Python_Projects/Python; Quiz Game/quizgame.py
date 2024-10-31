e9e4d38d0f458a4778918626cd0e8791aab70c9f
yuy(w)@amanarya1r <amanarya1r@gmail.com>
2024-10-31 23:47:57 +0530

    Already completed two projects 1st the Quiz game and 2nd the Number guessing game now I am working on Rock Paper Scissors game

--- Python_Projects/Python; Quiz Game/quizgame.py
+++ Python_Projects/Python; Quiz Game/quizgame.py
@@ -1,41 +1,49 @@
 print('Welcome to Quiz\n')
 playing = input("Do you want to play?(yes or no) \n: ")
 
-#to lower the letters of playing we can use .lower()
+#to lower the letters of playing we can use .lower() similar there .upper()
 
-if playing != "yes"  :
+if playing.lower() != "yes"  :
     quit() #use tab instead of spaces because it can cause some issue
 
-print("\n NOTE: All of your answer has to be in small case. \n")
-print("Okay! Let's start playing 😊")
 
-answer = input("What does CPU stand for? ")
+print("\nOkay! Let's start playing 😊")
+tally = 0
+
+answer = input("What does CPU stand for? ").lower()
 if answer == "central processing unit": 
     print("Correct! 🎉\n")
+    tally += 1
 else: 
     print("Incorrect ☹️\n")
 
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit": 
+if answer.lower() == "graphics processing unit": 
     print("Correct! 🎉\n")
+    tally += 1
 else: 
     print("Incorrect ☹️\n")
 
 answer = input("What does UPS stand for? ")
-if answer == "uninterruptible power supply": 
+if answer.lower() == "uninterruptible power supply": 
     print("Correct! 🎉\n")
+    tally += 1
 else: 
     print("Incorrect ☹️\n")
 
 answer = input("What does GPS stand for? ")
-if answer == "global positioning system": 
+if answer.lower() == "global positioning system": 
     print("Correct! 🎉\n")
+    tally += 1
 else: 
     print("Incorrect ☹️\n")
 
 answer = input("What does APU stand for? ")
-if answer == "accelerated processing unit": 
+if answer.lower() == "accelerated processing unit": 
     print("Correct! 🎉\n")
+    tally += 1
 else: 
     print("Incorrect ☹️\n")
     
+
+print(f"Youg got {tally} questions correct! And {5 - tally} incorrect. So you are {(tally/5)*100}% correct.")
