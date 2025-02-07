795f924a3700e34483903d9564610473421cc37a
yuy(w)@amanarya1r <amanarya1r@gmail.com>
2025-02-07 22:59:55 +0530

    In between turtle race I am stuck

--- TurtleRace/readme.md
+++ TurtleRace/readme.md
@@ -4,3 +4,7 @@ The whole program is made of python and Tkinter.
 
 ## Turtle Module
 A turtle module is built in python, and it allow user to basic 2d graphic operation with things called as turtle. It's like little header on the screen, it draw lines.
+Turtle racing uses a resolution of 500x500. The very middle of the game canvas is 0, 0 which means the top of canvas has 250, bottom of canvas has -250, right has the canvas -250 and the left of canvas has 250.
+
+When turtle run on the screen than it leaves a line which is called pen.
+To remove the residue line by using '.penup' property.
--- TurtleRace/turtlerace.py
+++ TurtleRace/turtlerace.py
@@ -1,20 +1,49 @@
 import turtle
+import time
+import random
 
 WIDTH, HEIGHT = 500, 500
-
-screen = turtle.Screen() # Describe screen for turtle modules
+COLORS = ['red', 'green', 'pink', 'blue', 'orange', 'yellow', 'purple', 'black', 'cyan', 'cyan']
 
 def get_number_of_racers():
     racers = 0
     while True:
-        racers = input("Enter the numer of racers (2-10): ")
+        racers = input("Enter the numer of racers (2-10) or 'q' to quit: ")
         if racers.isdigit():
             racers = int(racers)
             if 2 <= racers <= 10:
                 return racers
             else:
                 print("The given input is out of range! Please enter a number between 2 to 10.")
+        elif racers == 'q':
+            exit()
         else:
             print("Please entere a valid input!")
 
-get_number_of_racers()
+def create_turtles(colors):
+    turtles = []
+    spacingx = WIDTH // (len(colors) + 1)
+    for i, color in enumerate(colors):
+        racer = turtle.Turtle()
+        racer.color(color )
+        racer.shape('turtle')
+        racer.left(90) # it will move the turtle upwards
+        racer.penup()
+        racer.setpos()
+        racer.pendown()
+        turtles.append(racer)
+
+def init_turtle():
+    screen = turtle.Screen() # Describe screen for turtle modules
+    screen.setup(WIDTH, HEIGHT)
+    screen.title("🐢Turtle Racing🐢")
+
+
+racers = get_number_of_racers()
+init_turtle()
+
+random.shuffle(COLORS) # it will  shuffle the colors by which it become more randomly placed aroudn the list
+colors = COLORS[:racers]
+
+# racer = turtle.Turtle() # it create drawn object on the canvas
+create_turtles(colors)
